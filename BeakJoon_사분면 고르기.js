9aa52c074ab1ce7cb2234799f7c9f5b754d774c1
y00eunji <aine0227@naver.com>
2022-02-02 22:27:10 +0900

    14681
    readline() 버전도 같이 공부하기

---
+++ BeakJoon_사분면 고르기.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const [x, y] = fs.readFileSync(0).toString().trim().split('\n').map(Number);
+
+if(x > 0)
+    y > 0 ? console.log(1) : console.log(4)
+if(x < 0)
+    y > 0 ? console.log(2) : console.log(3)
