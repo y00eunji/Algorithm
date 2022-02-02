8783bbc13547a7dca7c2345b2045cad85bf3412f
y00eunji <aine0227@naver.com>
2022-02-02 22:14:23 +0900

    2753

---
+++ BeakJoon_윤년.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const year = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    console.log("1");
+} else {
+    console.log("0");
+}
