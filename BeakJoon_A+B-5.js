efc37e2a83e40ecf9aa59ad84e3cc328b1d44c0b
y00eunji <aine0227@naver.com>
2022-02-11 15:04:20 +0900

    109552

---
+++ BeakJoon_A+B-5.js
@@ -0,0 +1,17 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+let answer = '';
+let i = 0;
+
+while(true){
+    let nums = input[i].split(' ');
+
+    a = Number(nums[0]);
+    b = Number(nums[1]);
+
+    if(a==0 && b==0){
+        break;
+    }
+    console.log(a+b);
+    i++;
+}
