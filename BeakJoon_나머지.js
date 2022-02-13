a8a026b9c75a3fa256d348a7d598c21bb10b5648
y00eunji <aine0227@naver.com>
2022-02-13 15:59:47 +0900

    3052

---
+++ BeakJoon_나머지.js
@@ -0,0 +1,14 @@
+
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const userNum = [];
+
+input.forEach(x => {
+    const num = x % 42;
+
+    if (userNum.indexOf(num) === -1) {
+        userNum.push(num);
+    }
+});
+
+console.log(userNum.length);
