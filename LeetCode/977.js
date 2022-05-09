e54b0cb4f9e9f9bb369b9fbae8c6928a4dd6a5cf
y00eunji <aine0227@naver.com>
2022-05-09 17:26:16 +0900

    Squares of a Sorted Array

--- LeetCode/977.js
+++ LeetCode/977.js
@@ -2,5 +2,10 @@
  * 977. Squares of a Sorted Array
  * 각 배열요소에 제곱하고 오름차순으로 바꾸기
  */
-function sortedSquares(nums) {}
-console.log(sortedSquares([-4, -1, 0, 3, 10]));
+var sortedSquares = function (nums) {
+  let squareArr = nums.map((item) => item ** 2);
+  squareArr.sort((a, b) => {
+    return a - b;
+  });
+  return squareArr;
+};
