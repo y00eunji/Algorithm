a374b541e9fcf506d06fb11a39570b3ff0c58458
y00eunji <aine0227@naver.com>
2022-05-24 22:09:37 +0900

    155. minstack(스택)

---
+++ LeetCode/155.js
@@ -0,0 +1,48 @@
+var MinStack = function () {
+  this.stack = [];
+  this.minstack = []; //최솟값 스택
+};
+
+/**
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function (val) {
+  this.stack.push(val);
+  if (
+    this.minstack.length === 0 ||
+    val <= this.minstack[this.minstack.length - 1]
+  )
+    this.minstack.push(val);
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function () {
+  let val = this.stack.pop();
+  if (val === this.minstack[this.minstack.length - 1]) this.minstack.pop();
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function () {
+  return this.stack[this.stack.length - 1];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function () {
+  return this.minstack[this.minstack.length - 1];
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
