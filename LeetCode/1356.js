4261238de67c75b54e3556b5a2520fdbac7599f2
y00eunji <aine0227@naver.com>
2022-06-11 22:14:17 +0900

    1356. Sort Integers by The Number of 1 Bits(배열, 비트연산, 정렬)

---
+++ LeetCode/1356.js
@@ -0,0 +1,9 @@
+var sortByBits = function (arr) {
+  let bitArr = arr.map((v) => v.toString(2));
+
+  const countOne = bitArr.arr(/1/g);
+  return bits ? bits.length : 0;
+
+  console.log(countOne);
+};
+console.log(sortByBits([0, 1, 2, 3, 4, 5, 6, 7, 8]));
