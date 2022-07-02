5c1ae3b980e2a03e9374de0d8ee7a7d07f2311b2
y00eunji <aine0227@naver.com>
2022-07-02 22:21:09 +0900

    54번 주석 추가

--- LeetCode/54.js
+++ LeetCode/54.js
@@ -7,9 +7,9 @@ var spiralOrder = function (matrix) {
   while (matrix.length) {
     result.push(
       ...matrix.shift(), //첫번째줄 전체 넣기
-      ...matrix.map((a) => a.pop()),
-      ...(matrix.pop() ?? []).reverse(),
-      ...matrix.map((a) => a.shift()).reverse()
+      ...matrix.map((a) => a.pop()), //각 줄의 맨 뒤 뽑기
+      ...(matrix.pop() ?? []).reverse(), //맨 아랫줄 뒤집어서 넣기
+      ...matrix.map((a) => a.shift()).reverse() //마지막에 하나씩 앞에서 뽑아서 넣기
     );
   }
   return result.filter((el) => el != undefined);
