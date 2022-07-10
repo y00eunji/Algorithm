bc457afb1ebdc38802a7ca508627e6a97cd1b9be
y00eunji <aine0227@naver.com>
2022-07-10 17:05:29 +0900

    387. First Unique Character in a String(해쉬테이블, 문자열, 큐, 카운팅)

---
+++ LeetCode/387.js
@@ -0,0 +1,19 @@
+/**
+ * 387. First Unique Character in a String
+ * 해쉬테이블, 문자열, 큐, 카운팅
+ */
+
+var firstUniqChar = function (s) {
+  let map = new Map();
+  s = s.split("");
+  for (let i = 0; i < s.length; i++) {
+    let exist = map.get(s[i]) + 1 || 1;
+    map.set(s[i], exist);
+  }
+  for (const str of map.entries()) {
+    if (str[1] === 1) {
+      return s.indexOf(str[0]);
+    }
+  }
+};
+console.log(firstUniqChar((s = "loveleetcode")));
