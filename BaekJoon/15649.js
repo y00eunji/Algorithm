fbe1271a69b0bdd66659da22d8d63a174ae188a4
y00eunji <aine0227@naver.com>
2023-04-27 15:48:04 +0900

    N과 M (1) 새로운 풀이

--- BaekJoon/15649.js
+++ BaekJoon/15649.js
@@ -3,36 +3,29 @@
  제귀 종료 조건 : 길이가 M일때 종료 
  */
 
-const [N, M] = require("fs")
-  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
-  .toString()
-  .trim()
-  .split(" ")
-  .map((v) => +v);
+// const answer = [];
+// const array = Array.from({ length: N }, (_, i) => i + 1);
+// const visited = Array.from({ length: N }, (v) => false);
 
-const answer = [];
-const array = Array.from({ length: N }, (_, i) => i + 1);
-const visited = Array.from({ length: N }, (v) => false);
+// const getPermutations = (arr, M, permutation) => {
+//   if (permutation.length === M) {
+//     answer.push(permutation.join(" "));
+//     return;
+//   }
 
-const getPermutations = (arr, M, permutation) => {
-  if (permutation.length === M) {
-    answer.push(permutation.join(" "));
-    return;
-  }
+//   for (let i = 0; i < arr.length; i++) {
+//     if (visited[i]) continue;
 
-  for (let i = 0; i < arr.length; i++) {
-    if (visited[i]) continue;
+//     permutation.push(array[i]);
+//     visited[i] = true;
+//     getPermutations(arr, M, permutation);
+//     permutation.pop();
+//     visited[i] = false;
+//   }
+// };
+// getPermutations(array, M, []);
 
-    permutation.push(array[i]);
-    visited[i] = true;
-    getPermutations(arr, M, permutation);
-    permutation.pop();
-    visited[i] = false;
-  }
-};
-getPermutations(array, M, []);
-
-console.log(answer.join("\n"));
+// console.log(answer.join("\n"));
 
 // 순열구하기
 // const getPermutations = function (arr, selectNumber) {
@@ -47,3 +40,31 @@ console.log(answer.join("\n"));
 
 //   return results;
 // };
+
+const [N, M] = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
+  .toString()
+  .trim()
+  .split(' ')
+  .map((v) => +v);
+
+const numbers = Array.from({ length: N }, (_, i) => i + 1);
+
+const getPermutations = (arr, selectNumber) => {
+  const results = [];
+
+  if (selectNumber === 1) return arr.map((v) => [v]);
+
+  arr.forEach((fixed, index, origin) => {
+    const rest = [...origin.slice(0, index), ...origin.slice(index + 1)];
+    const permutations = getPermutations(rest, selectNumber - 1);
+    const attached = permutations.map((el) => [fixed, ...el]);
+    results.push(...attached);
+  });
+
+  return results;
+};
+
+const res = getPermutations(numbers, M);
+
+console.log(res.map((v) => v.join(' ')).join('\n'));
