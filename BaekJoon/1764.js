5a070c0b40cfbb368fbbc493ce23911c4ebc7ead
y00eunji <aine0227@naver.com>
2023-07-19 22:18:29 +0900

    듣보잡 (리팩)

--- BaekJoon/1764.js
+++ BaekJoon/1764.js
@@ -1,20 +1,14 @@
-const input = require("fs")
-  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
   .toString()
   .trim()
-  .split("\n");
+  .split('\n');
 
-const answer = [];
-const [n, m] = input[0].split(" ").map((v) => +v);
-const map = new Map();
+const [n, m] = input[0].split(' ').map(Number);
+
 const arr1 = input.slice(1, n + 1);
-const arr2 = input.slice(n + 1);
+const arr2 = new Set(input.slice(n + 1));
 
-arr1.forEach((v) => map.set(v, 0));
-arr2.forEach((v) => {
-  if (map.has(v)) answer.push(v);
-});
+const answer = arr1.filter((name) => arr2.has(name)).sort();
 
-answer.sort();
-
-console.log([answer.length, ...answer].join("\n"));
+console.log([answer.length, ...answer].join('\n'));
