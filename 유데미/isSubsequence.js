d34c81bc550d7ec70075d425f0f3c6eeda36adb0
y00eunji <aine0227@naver.com>
2023-12-08 19:49:11 +0900

    다중포인터

---
+++ 유데미/isSubsequence.js
@@ -0,0 +1,13 @@
+function isSubsequence(str1, str2) {
+  if (str1.length === 0) return true;
+  if (str2.length === 0) return false;
+  if (str2[0] === str1[0]) return isSubsequence(str1.slice(1), str2.slice(1));
+  return isSubsequence(str1, str2.slice(1));
+}
+
+console.log(
+  isSubsequence('hello', 'hello world'),
+  isSubsequence('sing', 'sting'),
+  isSubsequence('abc', 'abracadabra'),
+  isSubsequence('abc', 'acb')
+);
